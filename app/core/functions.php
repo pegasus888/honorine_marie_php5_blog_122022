4cfe2f18872c25a1d42ed51e7530f4e5d5a01760
pegasus888 <85719175+pegasus888@users.noreply.github.com>
2023-01-24 11:11:21 +0100

    Create DB: myblog_db

--- app/core/functions.php
+++ app/core/functions.php
@@ -3,9 +3,16 @@
 create_tables();
 function create_tables()
 {
-
+    // Make a connection with DB using a PDO Object
     $string = "mysql:hostname=localhost;";
     $con = new PDO($string, DBUSER, DBPASS);
 
-    print_r($con);
+    //print_r($con); (to print "PDO Object()" on index.php page)
+
+    // Create DB: myblog_db
+	$query = "create database if not exists ". DBNAME;
+	$stm = $con->prepare($query);
+	$stm->execute();
+
+
 }
