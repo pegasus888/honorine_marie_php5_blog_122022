69d73a5e15a4c7de89a9b8290fd73d15d448f132
pegasus888 <85719175+pegasus888@users.noreply.github.com>
2023-01-24 11:31:51 +0100

    core/functions.php->create table users if not exists

--- app/core/functions.php
+++ app/core/functions.php
@@ -3,16 +3,37 @@
 create_tables();
 function create_tables()
 {
-    // Make a connection with DB using a PDO Object
+    // Make a connection with a DB using a PDO Object
     $string = "mysql:hostname=localhost;";
     $con = new PDO($string, DBUSER, DBPASS);
 
     //print_r($con); (to print "PDO Object()" on index.php page)
 
-    // Create DB: myblog_db
+    // Create a DB if not exists: myblog_db
 	$query = "create database if not exists ". DBNAME;
 	$stm = $con->prepare($query);
 	$stm->execute();
 
+     // Select a DB
+	$query = "use ". DBNAME;
+	$stm = $con->prepare($query);
+	$stm->execute();
+
+    // Create table "users" if not exists: myblog_db
+    $query = "create table if not exists users(
+
+        id int primary key auto_increment,
+        username varchar(50) not null,
+        email varchar(100) not null,
+        password varchar(255) not null,
+        image varchar(1024) null,
+        date datetime default current_timestamp,
+        role varchar(10) not null,
+
+        key username (username),
+        key email (email)
+    )";
+	$stm = $con->prepare($query);
+	$stm->execute();
 
 }
